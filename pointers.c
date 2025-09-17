a1dc272226d3a3599c0f93b9480fd0ccb6514a87
mw224cg <138625041+mw224cg@users.noreply.github.com>
2025-09-17 11:55:32 +0200

    Added comments

--- pointers.c
+++ pointers.c
@@ -71,4 +71,41 @@ int main(void){
 
 '*': Dereferencer, *p gives value that p is pointing to
 '&': Address to, &a returns address of a
+
+1. Explain how you get the pointer addresses to the two char arrays (text1 and text2) and the
+ counter variable (count) in function work().
+
+ When initiallized (line 12,13) the variable 'text' points to the adress of the first char in the string.
+ To get the adress of the counter variable the '&'-sign is used.
+
+--> work(text1,text2,&counter) the parameters are pointers to the adresses containing the variables
+
+2. What does it mean to increment a pointer? What is the difference between incrementing the
+ pointer that points to the ASCII text string, and incrementing the pointer that points to the
+ integer array? In what way is the assembler code and the C code different?
+
+Incrementing a pointer (e.g text++) means that you move the pointer to the next element in the adress
+it points to. Incrementing the ASCII text string (text1,text2) pointers means that the
+pointer jumps 1 byte (size of char) whilst incrementing the integer array makes the pointer
+jump 4 bytes (size of an int).
+
+3. What is the difference between incrementing a pointer and incrementing a variable that a
+ pointer points to? Explain how your code is incrementing the count variable.
+
+Incrementing a pointer means to move where in the memory it points to,
+incrementing a variable that a pointer points to means to change the value 
+stored at the adress of the pointer. To increment the counter variable, it 
+needs to be dereferenced: (*counter)++
+
+4. Explain a statement in your code where you are dereferencing a pointer. What does this mean?
+ Explain by comparing it with the corresponding assembler code. 
+
+ Line 44: *list = *text; 
+
+ The char (1byte) that 'text' points to gets moved to the int (4byte) that 'list' points to
+ 
+ In assembler code:
+
+ sb t0, 0(a0)  #Load 1 byte to t0 from  memory adress given in a0 (the first)
+ sw t0, 0(a1)  #Store 4 bytes from t0 to the memory adress given in a1
 */
